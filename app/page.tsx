3956df0e618d9171e879001c32455742514caaff
Swaraj Patil <swarj.patil@iryscloud.com>
2025-01-09 00:55:11 +0530

    Added pricing section in home page

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import dataStocks from '@/public/media/data-stocks.jpg'
 import dataCrypto from '@/public/media/data-crypto.jpg'
 import dataCurrency from '@/public/media/data-currency.jpg'
 import dataCommodities from '@/public/media/data-comodities.jpg'
+import { FaCheck } from "react-icons/fa6";
 
 const performanceData = [
   { id: 1, name: 'Stocks' },
@@ -18,12 +19,51 @@ const performanceData = [
   { id: 4, name: 'Commodities' },
 ]
 
+const pricing = [
+  {
+    id: 1,
+    title: 'Monthly',
+    cost: 99,
+    tags: ['Most popular'],
+    benefits: [
+      'Unlimited access to the Universal Indicator',
+      'Real-time sentiment data',
+      'AI-driven predictions',
+      'Simple and intuitive design'
+    ]
+  },
+  {
+    id: 2,
+    title: 'Quarterly',
+    cost: 270,
+    benefits: [
+      'Unlimited access to the Universal Indicator',
+      'Real-time sentiment data',
+      'AI-driven predictions',
+      'Simple and intuitive design'
+    ]
+  },
+  {
+    id: 3,
+    title: 'Yearly',
+    cost: 960,
+    benefits: [
+      'Unlimited access to the Universal Indicator',
+      'Real-time sentiment data',
+      'AI-driven predictions',
+      'Simple and intuitive design'
+    ]
+  },
+]
+
 export default function Home() {
 
   const [selectedData, setSelectedData] = useState<string>('Stocks')
   const [selectedDataImage, setSelectedDataImage] = useState(dataStocks)
+  const [isMounted, setIsMounted] = useState<boolean>(false)
 
   useEffect(() => {
+    setIsMounted(true)
     switch (selectedData) {
       case 'Stocks':
         setSelectedDataImage(dataStocks)
@@ -39,6 +79,8 @@ export default function Home() {
         break
     }
   }, [selectedData])
+
+  if (!isMounted) return null
 
   return (
     <div className="">
@@ -114,6 +156,25 @@ export default function Home() {
             layout="fill"
             objectFit="cover"
           />
+        </div>
+      </section>
+
+      <section className="py-9 px-44">
+        <p className="font-[700] text-[22px] leading-7 mb-4">Pricing</p>
+
+        <div className="flex items-center gap-x-3 mb-10">
+          {pricing.map(eachPrice =>
+            <div className="w-[300px] h-[250px] rounded-xl border border-[#3D4A52] bg-[#1C2126] p-[24px]">
+              <div className="content__between mb-2">
+                <p className="font-[700] text-base">{eachPrice.title}</p>
+                {eachPrice.tags?.length && <div className="h-6 rounded-xl bg-[#1C8CD6] text-xs font-[500] px-3 py-1">{eachPrice.tags[0]}</div>}
+              </div>
+
+              <p className="text-4xl font-[900] mb-4">{`$${eachPrice.cost.toLocaleString()}`}<span className="font-[700] text-base ml-1">{`/${eachPrice.title.toLowerCase().slice(0, -2)}`}</span></p>
+
+              <div className="mb-3 text-[13px] font-[400]">{eachPrice.benefits?.map(benefit => <p className="flex items-center gap-x-3 mb-2"><FaCheck fontSize={18} />{benefit}</p>)}</div>
+            </div>
+          )}
         </div>
       </section>
     </div>
